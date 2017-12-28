461a65dfa33a575add62861cf4bdb664f74384c0
charlallison <soprinye@gmail.com>
2017-12-28 19:19:29 +0100

    Initial WPF application setup using odyssey

---
+++ ProType/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Odyssey.Controls;
+using Odyssey.Controls.Classes;
+
+namespace ProType {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+            SkinManager.SkinId = SkinId.Windows7;
+        }
+
+        private void OfficeBlue(object sender, System.Windows.RoutedEventArgs e) {
+            SkinManager.SkinId = SkinId.OfficeBlue;
+        }
+
+        private void OfficeBlack(object sender, System.Windows.RoutedEventArgs e) {
+            SkinManager.SkinId = SkinId.OfficeBlack;
+        }
+
+        private void Windows7(object sender, System.Windows.RoutedEventArgs e) {
+            SkinManager.SkinId = SkinId.Windows7;
+        }
+
+        private void Vista(object sender, System.Windows.RoutedEventArgs e) {
+            SkinManager.SkinId = SkinId.Vista;
+        }
+
+        private void OfficeSilver(object sender, System.Windows.RoutedEventArgs e) {
+            SkinManager.SkinId = SkinId.OfficeSilver;
+        }
+    }
+}
